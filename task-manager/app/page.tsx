2b9774b0ed66306df8a5a6e741d6cb3e423dcf58
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-09-02 17:14:34 +0530

    feat:implemented task type and added the addtask fn

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -1,7 +1,28 @@
+'use client' 
+import { useState } from "react";
+import type { Task } from "./types";
+
 export default function Home() {
+  const [tasks,setTasks] = useState<Task[]>([]);
+  const [title,setTitle] = useState('')
+
+  const addTask = ()=>{
+    if(title.trim()==='') return;
+
+    const newTask:Task = {
+      id:Date.now(),
+      title:title,
+      completed:false
+    }
+    setTasks([...tasks,newTask]);
+    setTitle("");
+  }
+   
   return (
     <main>
       <h1>Task Manager</h1>
+      <input type="text"  placeholder="add a task" value={title} onChange={(e)=>setTitle(e.target.value)} />
+      <button onClick={addTask}>add</button>
     </main>
   );
 }
