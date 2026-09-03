abd3f74bcf2748775a6fed20ea036e885bb4413a
vyasan-6-6 <vyasanksparasakthy@gmail.com>
2026-09-03 11:24:44 +0530

    feat:implemented task completed/undo toggle fn with line streak

--- task-manager/app/page.tsx
+++ task-manager/app/page.tsx
@@ -17,7 +17,16 @@ export default function Home() {
     setTasks([...tasks,newTask]);
     setTitle("");
   }
-   
+    const toggleTask = (id: number) => {
+  setTasks(
+    tasks.map((task) =>
+      task.id === id
+        ? { ...task, completed: !task.completed }
+        : task
+    )
+  );
+};
+  
   return (
     <main>
       <h1>Task Manager</h1>
@@ -26,9 +35,18 @@ export default function Home() {
 
       <ul>
   {tasks.map((task) => (
-    <li key={task.id}>
-      {task.title}
-    </li>
+   <li key={task.id}>
+ <span
+  style={{
+    textDecoration: task.completed ? "line-through" : "none",
+  }}
+>
+  {task.title}
+</span>
+ <button onClick={() => toggleTask(task.id)}>
+    {task.completed ? " Undo" : " Complete"}
+  </button>
+</li>
   ))}
 </ul>
     </main>
